56387e34126eb9f143749ee53edd6fdfe4859a28
Umama Saif <114926750+UmamaSaif@users.noreply.github.com>
2024-08-09 08:55:02 -0700

    Create Day19.py

---
+++ Day19.py
@@ -0,0 +1,21 @@
+"""
+You are given two arrays of equal length, nums1 and nums2.
+
+Each element in nums1 has been increased (or decreased in the case of negative) by an integer, represented by the variable x.
+
+As a result, nums1 becomes equal to nums2. Two arrays are considered equal when they contain the same integers with the same frequencies.
+
+Return the integer x.
+"""
+class Solution(object):
+    def addedInteger(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        nums1=sorted(nums1)
+        nums2=sorted(nums2)
+
+        return nums2[0]-nums1[0]
+        
