18aa5c432899283a17c931c9a4e52810d7a0a6ca
Umama Saif <114926750+UmamaSaif@users.noreply.github.com>
2024-08-15 11:40:53 -0700

    Create Day25.py

---
+++ Day25.py
@@ -0,0 +1,20 @@
+"""
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+"""
+
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        arr=[]
+        
+        for i in range(n):
+            arr.append(nums[i])
+            arr.append(nums[i+n])
+        return arr
+        
