09f80531315b59e909c7979816f3d355b3e6f46f
Umama Saif <114926750+UmamaSaif@users.noreply.github.com>
2024-08-19 08:32:49 -0700

    Create Day28.py

---
+++ Day28.py
@@ -0,0 +1,27 @@
+"""
+Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+"""
+class Solution(object):
+    
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique_nums=[]
+        for num in nums:
+            if num not in unique_nums:
+                unique_nums.append(num)
+        maximum=0
+        if len(unique_nums) >= 3:
+            for i in range(0,3):
+                if unique_nums:
+                    maximum=max(unique_nums)
+                while maximum in unique_nums:
+                    unique_nums.remove(maximum)
+        else:
+            if unique_nums:
+                return max(unique_nums)
+        return maximum
+
+        
