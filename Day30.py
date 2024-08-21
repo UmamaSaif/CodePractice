0d1a6ac21e3839d58e337d841152b7efc0ac7d77
Umama Saif <114926750+UmamaSaif@users.noreply.github.com>
2024-08-21 11:52:21 -0700

    Create Day30.py

---
+++ Day30.py
@@ -0,0 +1,24 @@
+"""
+Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+"""
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+
+        child=0
+        cookie=0
+
+        while child <len(g) and cookie <len(s):
+            if s[cookie] >= g[child]:
+                child +=1
+            cookie+=1
+        return child
+        
